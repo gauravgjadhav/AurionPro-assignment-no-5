61918ea0e131a7565851eeec4225e6d0b6f72c27
gauravgjadhav <92154289+gauravgjadhav@users.noreply.github.com>
2022-07-02 20:34:11 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Model
+{
+    internal class Product
+    {
+        private int _id = 0;
+        private string _name = null;
+        private int _price = 0;
+        private double _discountPercentage =0;
+        public double discontPrice=0;
+        public double calculatePrice=0;
+
+        public int Id 
+        { 
+            get 
+            { 
+                return _id; 
+            } 
+            set 
+            { 
+                _id = value; 
+            }
+        }
+        public string Name
+        {
+            get
+            { 
+                return _name;
+            }
+            set 
+            { 
+                _name = value;
+            }
+        }
+        public int Price
+        {
+            get 
+            { 
+                return _price; 
+            }
+            set 
+            { 
+                _price = value;
+            }
+        }
+        public double DiscountPercentage
+        {
+            get
+            {
+                return _discountPercentage;
+            }
+            set 
+            {
+                _discountPercentage = value;
+            }
+        }
+        
+        public double DiscountCalculate()
+        {
+            _discountPercentage = _discountPercentage / 100;
+            discontPrice = _price * _discountPercentage;
+            calculatePrice = _price - discontPrice;
+            return calculatePrice;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ProductApp.Model;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Product rice;
+        rice = new Product();
+        rice.Id=1001;
+        rice.Name = "Rice";
+        rice.Price=30;
+        rice.DiscountPercentage = 2;
+        PrintDetails(rice); 
+
+        Product suger;
+        suger = new Product();
+        suger.Id=1002;
+        suger.Name = "Suger";
+        suger.Price = 30;
+        suger.DiscountPercentage =10;
+        PrintDetails(suger);
+    }
+    public static  void PrintDetails(Product productPrint)
+    {
+        Console.WriteLine("Product Id Is : " + productPrint.Id);
+        Console.WriteLine("Product Name Is : " + productPrint.Name);
+        Console.WriteLine("Product Price IS : " + productPrint.Price + ".Rs kg");
+        Console.WriteLine("Discount is : " + productPrint.DiscountPercentage + "%");
+        Console.WriteLine("Product After Discount Price " + productPrint.DiscountCalculate() + ".Rs");
+        Console.WriteLine("_______________________________________");
+
+    }
+}
